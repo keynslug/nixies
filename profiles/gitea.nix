9d7eb76dfed243c28b9167f69a4ec4127e64fa6a
Andrew Mayorov <encube.ul@gmail.com>
2021-12-16 13:43:49 +0300

    Add gitea profile w/ postresql storage

---
+++ profiles/gitea.nix
@@ -0,0 +1,62 @@
+{config, lib, pkgs, ...}:
+
+with lib;
+
+let
+    giteaDomain = "git.keynfawk.es";
+    # giteaPort = 3000;
+    defaultGateway = config.networking.defaultGateway.interface;
+in
+{
+    services.gitea = {
+        enable = true;
+        enableUnixSocket = true;
+        database.type = "postgres";
+        database.createDatabase = true;
+        domain = giteaDomain;
+        rootUrl = "https://${giteaDomain}/";
+        disableRegistration = true;
+        settings = {
+            service = {
+                REQUIRE_SIGNIN_VIEW = true;
+            };
+        };
+    };
+
+    services.nginx = let
+        giteaSocket = config.services.gitea.settings.server.HTTP_ADDR;
+    in {
+        enable = true;
+        package = pkgs.nginxMainline;
+        recommendedTlsSettings = true;
+        recommendedProxySettings = true;
+        virtualHosts = listToAttrs [
+            (nameValuePair giteaDomain {
+                forceSSL = true;
+                enableACME = true;
+                locations."/" = {
+                    proxyPass = "http://unix:${giteaSocket}:/";
+                };
+            })
+        ];
+    };
+
+    security.acme = {
+        # NOTE
+        # Do not forget to put correct email address here
+        # email = "keynslug@mail";
+        acceptTerms = true;
+        renewInterval = "monthly";
+    };
+
+    networking.firewall.interfaces = listToAttrs [
+        (nameValuePair defaultGateway {
+            allowedTCPPorts = [ 80 443 ];
+        })
+    ];
+    #// optionalAttrs config.networking.wireguard.enable {
+    #    wg0 = {
+    #        allowedTCPPorts = [ giteaPort ];
+    #    };
+    #};
+}
